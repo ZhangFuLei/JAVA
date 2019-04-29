d8355d30ff3da67d1fa78017111907c5289b4807
zhangfulei <2690370788@qq.com>
2019-04-29 18:56:12 +0800

    把本地工程添加到git21

--- JavaZfl/src/com/itcast/TestJava.java
+++ JavaZfl/src/com/itcast/TestJava.java
@@ -4,4 +4,7 @@ public class TestJava {
     public static void main(String[] args) {
         System.out.println("张福垒");
     }
+    public void method(){
+        System.out.println("裴小娜");
+    }
 }
